54d822c5fa6c3eb1ad2677762833c88dc704faf7
JosueEC <josueev.cruz@gmail.com>
2025-05-07 20:05:11 -0600

    [CHORE](users.repository): Inject the default repository in the class constructor

--- src/users/repository/users.repository.ts
+++ src/users/repository/users.repository.ts
@@ -1,13 +1,24 @@
 import { DeleteResultResponse } from 'src/common/dto/response';
 import { CreateRateRequest } from 'src/grpc/rooms/rates.pb';
-import { QueryRunner, FindOptionsWhere } from 'typeorm';
+import { QueryRunner, FindOptionsWhere, Repository } from 'typeorm';
 import { User } from '../entity/user.entity';
 import { IUsersRepository } from './interfaces/users.repository.interface';
+import { InjectRepository } from '@nestjs/typeorm';
 
 export class UsersRepository implements IUsersRepository {
+  private usersRepository: Repository<User>;
+
+  constructor(
+    @InjectRepository(User)
+    private readonly defaultRepository: Repository<User>,
+  ) {
+    this.usersRepository = this.defaultRepository;
+  }
+
   setQueryRunner(queryRunner: QueryRunner): void {
     throw new Error('Method not implemented.');
   }
+
   find(): Promise<User[]> {
     throw new Error('Method not implemented.');
   }
