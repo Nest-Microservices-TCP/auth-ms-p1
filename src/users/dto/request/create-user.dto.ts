099b6706b4e89c2f210bd2c6c910348f1e62aad2
JosueEC <josueev.cruz@gmail.com>
2025-05-08 22:29:05 -0600

    [CHORE](users): Delete the dto directory

--- src/users/dto/request/create-user.dto.ts
+++
@@ -1,8 +0,0 @@
-import { CreateUserRequest } from 'src/grpc/auth/users.pb';
-
-export class CreateUserDto implements CreateUserRequest {
-  nickname: string;
-  email: string;
-  password: string;
-  full_name: string;
-}
