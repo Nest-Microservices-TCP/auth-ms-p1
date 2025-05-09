94d7ea022bf6520510cb70f756d68d653c35c965
JosueEC <josueev.cruz@gmail.com>
2025-05-08 22:20:15 -0600

    [CHORE](users.service): Add the save service method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './repository/users.repository';
+import { CreateUserDto } from './dto/request/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
+
+  async save(request: CreateUserDto): Promise<void> {
+    this.usersRepository.save(request);
+  }
 }
