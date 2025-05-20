6aa2e67e3cd9d85ba2ec81ef01da1e281e14cb60
JosueEC <josueev.cruz@gmail.com>
2025-05-19 21:42:05 -0600

    [CHORE](users.module): Inject the providers for the module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { UsersService } from './users.service';
+import { UsersRepository } from './repository/users.repository';
+
 import { User } from './entity/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersRepository, UsersService],
 })
 export class UsersModule {}
