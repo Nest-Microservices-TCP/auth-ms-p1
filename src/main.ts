6f1cd1928c8323905773e60c99dbf2c52783d703
JosueEC <josueev.cruz@gmail.com>
2025-05-19 21:47:15 -0600

    [BUILD](main.ts): Define a basic configuration for gRPC microservice

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Auth-MS');
 
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const grpcApp = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        url: '',
+        package: [],
+        protoPath: [],
+        loader: {
+          includeDirs: [],
+          keepCase: true,
+          enums: String,
+          arrays: true,
+        },
+      },
+    },
+  );
 }
 bootstrap();
