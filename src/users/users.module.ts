50cad50a82a7978dd5f330310f812116956b2628
JosueEC <josueev.cruz@gmail.com>
2025-05-19 21:42:31 -0600

    [CHORE](users.module): Inject the controllers for the module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 import { UsersRepository } from './repository/users.repository';
 
 import { User } from './entity/user.entity';
@@ -9,5 +10,6 @@ import { User } from './entity/user.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersRepository, UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
