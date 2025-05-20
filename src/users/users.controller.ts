98a3c03e362d75cf1addc0a4580d597108b364dd
JosueEC <josueev.cruz@gmail.com>
2025-05-19 21:36:41 -0600

    [CHORE](users.controller): Implements the users-service-controller interface from proto types

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,43 @@
+import { Observable } from 'rxjs';
 import { Controller } from '@nestjs/common';
+
+import {
+  User,
+  FindUsersResponse,
+  CreateUserRequest,
+  FindOneUserRequest,
+  FindUsersByIdsRequest,
+  UsersServiceController,
+  UsersServiceControllerMethods,
+} from 'src/grpc/auth/users.pb';
+
 import { UsersService } from './users.service';
 
 @Controller()
-export class UsersController {
+@UsersServiceControllerMethods()
+export class UsersController implements UsersServiceController {
   constructor(private readonly userService: UsersService) {}
+
+  save(request: CreateUserRequest): void {
+    this.userService.save(request);
+  }
+  findOne(
+    request: FindOneUserRequest,
+  ): Promise<User> | Observable<User> | User {
+    return this.userService.findOne(request);
+  }
+  find():
+    | Promise<FindUsersResponse>
+    | Observable<FindUsersResponse>
+    | FindUsersResponse {
+    return this.userService.find();
+  }
+  findByIds(
+    request: FindUsersByIdsRequest,
+  ):
+    | Promise<FindUsersResponse>
+    | Observable<FindUsersResponse>
+    | FindUsersResponse {
+    return this.userService.findByIds(request);
+  }
 }
